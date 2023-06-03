2cbf42644d3a7700a96cdd4d1ff421b5cc0ba034
Raj <rajkiran.palla@gmail.com>
2023-06-03 23:10:24 +0200

    remove console.logs in prod

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const heading2 = React.createElement("h1",[],"heading2");
 
 const div = React.createElement("div",[],[heading1,heading2]);
 
-
+// will be removed in build as we are using bael plugin to remove console.logs
+console.log(div);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
