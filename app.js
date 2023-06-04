9030d41161983aaf6bf9fe7bcde1f7a506092ce8
Raj <rajkiran.palla@gmail.com>
2023-06-04 15:34:57 +0200

    convert to jsx and render

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1",[],"heading1");
-const heading2 = React.createElement("h1",[],"heading2");
 
-const div = React.createElement("div",[],[heading1,heading2]);
+// react element
+const header1 = <h1>heading1</h1>;
+
+// functional component
+const Header2 = () => (
+    <h1>heading2</h1>
+)
+
+// functional component uses react element and another react functional component
+const HeaderElement = () => (
+    <div>
+    {header1}
+    <Header2/>
+    </div>
+)
 
 // will be removed in build as we are using bael plugin to remove console.logs
-console.log(div);
+console.log(HeaderElement);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(div);
+root.render(<HeaderElement/>);
