9b26e16bf48148c9f1b3202090ccd20ff102680b
Raj <rajkiran.palla@gmail.com>
2023-06-26 18:44:06 +0200

    Format code with prettier

--- src/Body.js
+++ src/Body.js
@@ -1,33 +1,47 @@
 import RestrauntCard from "./RestaurantCard";
-import {restrautList} from "./constants";
+import { restrautList } from "./constants";
 import { useState } from "react";
 
 const filterRestaurants = (searchText) => {
-    return restrautList.filter(restaurant => restaurant.data.name.toLowerCase().includes(searchText));
-}
+  return restrautList.filter((restaurant) =>
+    restaurant.data.name.toLowerCase().includes(searchText)
+  );
+};
 
 const Body = () => {
-    const [searchText, setSearchText] = useState("");
-    const [restaurants, setRestaurants] = useState(restrautList);
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(restrautList);
 
-    return (
-       <>
-       <div className="search-container">
-        <input type="search" placeHolder="search" value={searchText} onChange={(e) => {
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="search"
+          placeHolder="search"
+          value={searchText}
+          onChange={(e) => {
             setSearchText(e.target.value);
-        }}></input>
-        <button type="button" onClick={() => {
-           const filteredRestaurants = filterRestaurants(searchText);
-           setRestaurants(filteredRestaurants);
-        }}>search</button>
-       </div>
-        <div className="restaurant-list">
+          }}
+        ></input>
+        <button
+          type="button"
+          onClick={() => {
+            const filteredRestaurants = filterRestaurants(searchText);
+            setRestaurants(filteredRestaurants);
+          }}
+        >
+          search
+        </button>
+      </div>
+      <div className="restaurant-list">
         {restaurants.map((restaurant) => {
-          return <RestrauntCard {...restaurant.data} key={restaurant.data.id} />;
+          return (
+            <RestrauntCard {...restaurant.data} key={restaurant.data.id} />
+          );
         })}
       </div>
-       </> 
-    );
-  };
+    </>
+  );
+};
 
-  export default Body;
+export default Body;
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,28 @@
 const Title = () => (
-    <a href="/">
-     <img className="logo" alt="logo" src="https://st2.depositphotos.com/1037759/10068/v/950/depositphotos_100680876-stock-illustration-food-logo-template-home-food.jpg" />
-    </a>
-  );
-  
-  // Composing Comopnentss
-  const Header = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://st2.depositphotos.com/1037759/10068/v/950/depositphotos_100680876-stock-illustration-food-logo-template-home-food.jpg"
+    />
+  </a>
+);
 
-  export default Header;
+// Composing Comopnentss
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
