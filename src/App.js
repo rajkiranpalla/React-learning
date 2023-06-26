d3eb6f940b1a95c1a4a701ad2c8d40dce0abe7bb
Raj <rajkiran.palla@gmail.com>
2023-06-26 18:41:53 +0200

    Refactor code into individual files and add search

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import Footer from "./Footer";
+import Body from "./Body";
+
+/**
+     Header
+        - Logo(Title)
+        - Nav Items(Right Side)
+        - Cart
+     Body 
+        - Search bar
+        - RestrauntList
+          - RestaurantCard (many cards)
+              - Image
+              - Name
+              - Rating
+              - Cusines
+     Footer
+      - links
+      - Copyright
+    
+    */
+
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
